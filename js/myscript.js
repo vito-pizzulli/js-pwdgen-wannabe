7d021c3726dc9226100d4f95eee31eb1813f7799
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-11 17:43:32 +0200

    styling completed

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
 const userName = prompt("Inserisci il tuo nome");
 const userSurname = prompt("Inserisci il tuo cognome");
 const userFavoriteColor = prompt("Inserisci il tuo colore preferito");
-document.getElementById("outputPassword").innerHTML = 'La tua password estremamente sicura è ' + userName + userSurname + userFavoriteColor + 23;
+document.getElementById("outputPassword").innerHTML = 'La password generata esclusivamente per te è: ' + userName + userSurname + userFavoriteColor + 23;
