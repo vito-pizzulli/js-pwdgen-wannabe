0f752c5d529c8eba2302c54ae543c8d716b31a23
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-12 10:45:12 +0200

    small change done after exercize correction

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
 const userName = prompt("Inserisci il tuo nome");
 const userSurname = prompt("Inserisci il tuo cognome");
 const userFavoriteColor = prompt("Inserisci il tuo colore preferito");
-document.getElementById("outputPassword").innerHTML = 'La password generata esclusivamente per te è: ' + userName + userSurname + userFavoriteColor + 23;
+document.getElementById("outputPassword").innerHTML += " " + userName + userSurname + userFavoriteColor + 23;
